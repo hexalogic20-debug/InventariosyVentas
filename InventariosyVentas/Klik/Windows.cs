5e558f89e82928645305bdf9fcd1b76e10854af4
Emanuel <emanuelarbo14@gmail.com>
2026-01-18 10:12:51 -0500

    h

---
+++ InventariosyVentas/Klik/Windows.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Forms;
+
+namespace Klik
+{
+    internal class Windows
+    {
+        public static object Forms { get; internal set; }
+
+        internal class Forms
+        {
+            internal class v1
+            {
+                internal class EntryLib
+                {
+                    internal class ELButton
+                    {
+                        public ELButton()
+                        {
+                        }
+
+                        public AnchorStyles Anchor { get; internal set; }
+                        public object BackgroundStyle { get; internal set; }
+                    }
+
+                    internal class ELEntryBox
+                    {
+                        public ELEntryBox()
+                        {
+                        }
+                    }
+
+                    internal class ELButtonBackgroundStyle
+                    {
+                    }
+                }
+            }
+        }
+    }
+}
