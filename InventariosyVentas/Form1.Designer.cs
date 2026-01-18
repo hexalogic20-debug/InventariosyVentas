7c9561d725dde4f3375d9f316823f0d71777e2a4
hexalogic20-debu <hexalogic20@gmail.com>
2026-01-18 02:29:05 -0500

    se mejoro login

--- InventariosyVentas/Form1.Designer.cs
+++ InventariosyVentas/Form1.Designer.cs
@@ -33,18 +33,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.lblolvidopassword = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btnIngresar = new System.Windows.Forms.Button();
-            this.panelPassword = new System.Windows.Forms.Panel();
+            this.btnIngresar = new Klik.Windows.Forms.v1.EntryLib.ELButton();
+            this.textBoxUsuario = new Klik.Windows.Forms.v1.EntryLib.ELEntryBox();
+            this.textBoxPassword = new Klik.Windows.Forms.v1.EntryLib.ELEntryBox();
             this.pictureBoxOculo = new System.Windows.Forms.PictureBox();
             this.pictureBoxVisible = new System.Windows.Forms.PictureBox();
-            this.textBoxPassword = new System.Windows.Forms.TextBox();
-            this.panelUsuario = new System.Windows.Forms.Panel();
-            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panelPassword.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnIngresar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxUsuario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxPassword)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOculo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVisible)).BeginInit();
-            this.panelUsuario.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -61,7 +61,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(249, 264);
+            this.label2.Location = new System.Drawing.Point(246, 262);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 15);
             this.label2.TabIndex = 3;
@@ -71,7 +71,7 @@
             // 
             this.lblolvidopassword.AutoSize = true;
             this.lblolvidopassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(156)))), ((int)(((byte)(241)))));
-            this.lblolvidopassword.Location = new System.Drawing.Point(284, 338);
+            this.lblolvidopassword.Location = new System.Drawing.Point(284, 334);
             this.lblolvidopassword.Name = "lblolvidopassword";
             this.lblolvidopassword.Size = new System.Drawing.Size(140, 13);
             this.lblolvidopassword.TabIndex = 4;
@@ -89,72 +89,119 @@
             // 
             // btnIngresar
             // 
-            this.btnIngresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(0)))), ((int)(((byte)(31)))));
-            this.btnIngresar.ForeColor = System.Drawing.Color.White;
-            this.btnIngresar.Location = new System.Drawing.Point(314, 377);
+            this.btnIngresar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIngresar.BackgroundStyle.GradientAngle = 0F;
+            this.btnIngresar.BackgroundStyle.GradientEndColor = System.Drawing.Color.OrangeRed;
+            this.btnIngresar.BackgroundStyle.GradientStartColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnIngresar.BackgroundStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid;
+            this.btnIngresar.BackgroundStyle.SolidColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(0)))), ((int)(((byte)(31)))));
+            this.btnIngresar.BorderStyle.SolidColor = System.Drawing.Color.Teal;
+            this.btnIngresar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnIngresar.DropDownArrowColor = System.Drawing.Color.White;
+            this.btnIngresar.EnableThemes = false;
+            this.btnIngresar.FlashStyle.GradientEndColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnIngresar.FlashStyle.GradientStartColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnIngresar.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid;
+            this.btnIngresar.FlashStyle.SolidColor = System.Drawing.Color.OrangeRed;
+            this.btnIngresar.ForegroundImageStyle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIngresar.Location = new System.Drawing.Point(286, 366);
             this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(79, 31);
-            this.btnIngresar.TabIndex = 7;
-            this.btnIngresar.Text = "Ingresar";
-            this.btnIngresar.UseVisualStyleBackColor = false;
-            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            this.btnIngresar.Size = new System.Drawing.Size(137, 33);
+            this.btnIngresar.StateStyles.HoverStyle.BackgroundGradientEndColor = System.Drawing.Color.YellowGreen;
+            this.btnIngresar.StateStyles.HoverStyle.BackgroundGradientStartColor = System.Drawing.Color.YellowGreen;
+            this.btnIngresar.StateStyles.HoverStyle.BackgroundSolidColor = System.Drawing.Color.YellowGreen;
+            this.btnIngresar.StateStyles.HoverStyle.BorderSolidColor = System.Drawing.Color.YellowGreen;
+            this.btnIngresar.TabIndex = 27;
+            this.btnIngresar.TextStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnIngresar.TextStyle.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.TextStyle.ForeColor = System.Drawing.Color.White;
+            this.btnIngresar.TextStyle.Text = "Ingresar";
+            this.btnIngresar.TextStyle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnIngresar.VisualStyle = Klik.Windows.Forms.v1.EntryLib.ButtonVisualStyles.Custom;
             // 
-            // panelPassword
+            // textBoxUsuario
             // 
-            this.panelPassword.Controls.Add(this.pictureBoxOculo);
-            this.panelPassword.Controls.Add(this.pictureBoxVisible);
-            this.panelPassword.Controls.Add(this.textBoxPassword);
-            this.panelPassword.Location = new System.Drawing.Point(246, 284);
-            this.panelPassword.Name = "panelPassword";
-            this.panelPassword.Size = new System.Drawing.Size(219, 33);
-            this.panelPassword.TabIndex = 8;
+            this.textBoxUsuario.AutoSize = false;
+            this.textBoxUsuario.CaptionStyle.CaptionSize = 0;
+            this.textBoxUsuario.CaptionStyle.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid;
+            this.textBoxUsuario.CaptionStyle.FlashStyle.SolidColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(0)))), ((int)(((byte)(31)))));
+            this.textBoxUsuario.CaptionStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack;
+            this.textBoxUsuario.CaptionStyle.TextStyle.ForeColor = System.Drawing.Color.White;
+            this.textBoxUsuario.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBoxUsuario.EditBoxStyle.BackColor = System.Drawing.Color.White;
+            this.textBoxUsuario.EditBoxStyle.BorderStyle.BorderType = Klik.Windows.Forms.v1.Common.BorderTypes.None;
+            this.textBoxUsuario.EditBoxStyle.BorderStyle.EdgeRadius = 10;
+            this.textBoxUsuario.EditBoxStyle.FlashBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(221)))), ((int)(((byte)(0)))), ((int)(((byte)(31)))));
+            this.textBoxUsuario.EditBoxStyle.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsuario.EditBoxStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxUsuario.EditBoxStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack;
+            this.textBoxUsuario.Location = new System.Drawing.Point(241, 211);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(219, 38);
+            this.textBoxUsuario.TabIndex = 451;
+            this.textBoxUsuario.ValidationStyle.PasswordChar = '\0';
+            this.textBoxUsuario.Value = "";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.CaptionStyle.CaptionSize = 0;
+            this.textBoxPassword.CaptionStyle.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid;
+            this.textBoxPassword.CaptionStyle.FlashStyle.SolidColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(240)))), ((int)(((byte)(191)))));
+            this.textBoxPassword.CaptionStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack;
+            this.textBoxPassword.CaptionStyle.StateStyles.DisabledStyle.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textBoxPassword.CaptionStyle.StateStyles.FocusStyle.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textBoxPassword.CaptionStyle.StateStyles.HoverStyle.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textBoxPassword.CaptionStyle.TextStyle.ForeColor = System.Drawing.Color.White;
+            this.textBoxPassword.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBoxPassword.EditBoxStyle.BackColor = System.Drawing.Color.White;
+            this.textBoxPassword.EditBoxStyle.BorderStyle.BorderType = Klik.Windows.Forms.v1.Common.BorderTypes.None;
+            this.textBoxPassword.EditBoxStyle.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.EditBoxStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxPassword.EditBoxStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack;
+            this.textBoxPassword.EditBoxStyle.StateStyles.DisabledStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textBoxPassword.EditBoxStyle.StateStyles.FocusStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textBoxPassword.EditBoxStyle.StateStyles.HoverStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.textBoxPassword.Location = new System.Drawing.Point(241, 280);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(219, 38);
+            this.textBoxPassword.TabIndex = 454;
+            this.textBoxPassword.ValidationStyle.PasswordChar = '\0';
+            this.textBoxPassword.ValidationStyle.UseSystemPasswordChar = true;
+            this.textBoxPassword.Value = "";
             // 
             // pictureBoxOculo
             // 
             this.pictureBoxOculo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxOculo.Image")));
-            this.pictureBoxOculo.Location = new System.Drawing.Point(174, 8);
+            this.pictureBoxOculo.Location = new System.Drawing.Point(414, 291);
             this.pictureBoxOculo.Name = "pictureBoxOculo";
             this.pictureBoxOculo.Size = new System.Drawing.Size(37, 19);
             this.pictureBoxOculo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxOculo.TabIndex = 14;
+            this.pictureBoxOculo.TabIndex = 456;
             this.pictureBoxOculo.TabStop = false;
-            this.pictureBoxOculo.Click += new System.EventHandler(this.pictureBoxOculo_Click);
+            this.pictureBoxOculo.Click += new System.EventHandler(this.pictureBoxOculo_Click_1);
             // 
             // pictureBoxVisible
             // 
             this.pictureBoxVisible.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxVisible.Image")));
-            this.pictureBoxVisible.Location = new System.Drawing.Point(174, 7);
+            this.pictureBoxVisible.Location = new System.Drawing.Point(414, 290);
             this.pictureBoxVisible.Name = "pictureBoxVisible";
             this.pictureBoxVisible.Size = new System.Drawing.Size(37, 20);
             this.pictureBoxVisible.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBoxVisible.TabIndex = 13;
+            this.pictureBoxVisible.TabIndex = 455;
             this.pictureBoxVisible.TabStop = false;
             this.pictureBoxVisible.Visible = false;
-            this.pictureBoxVisible.Click += new System.EventHandler(this.pictureBoxVisible_Click);
+            this.pictureBoxVisible.Click += new System.EventHandler(this.pictureBoxVisible_Click_1);
             // 
-            // textBoxPassword
+            // textBox1
             // 
-            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxPassword.Location = new System.Drawing.Point(5, 6);
-            this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(208, 13);
-            this.textBoxPassword.TabIndex = 2;
-            // 
-            // panelUsuario
-            // 
-            this.panelUsuario.Controls.Add(this.textBoxUsuario);
-            this.panelUsuario.Location = new System.Drawing.Point(247, 211);
-            this.panelUsuario.Name = "panelUsuario";
-            this.panelUsuario.Size = new System.Drawing.Size(219, 33);
-            this.panelUsuario.TabIndex = 9;
-            // 
-            // textBoxUsuario
-            // 
-            this.textBoxUsuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxUsuario.Location = new System.Drawing.Point(5, 6);
-            this.textBoxUsuario.Name = "textBoxUsuario";
-            this.textBoxUsuario.Size = new System.Drawing.Size(208, 13);
-            this.textBoxUsuario.TabIndex = 2;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(249, 287);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 25);
+            this.textBox1.TabIndex = 457;
+            this.textBox1.Visible = false;
             // 
             // Form1
             // 
@@ -162,8 +209,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(716, 511);
-            this.Controls.Add(this.panelUsuario);
-            this.Controls.Add(this.panelPassword);
+            this.Controls.Add(this.pictureBoxVisible);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBoxOculo);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsuario);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblolvidopassword);
@@ -176,12 +226,11 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panelPassword.ResumeLayout(false);
-            this.panelPassword.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnIngresar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxUsuario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textBoxPassword)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOculo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxVisible)).EndInit();
-            this.panelUsuario.ResumeLayout(false);
-            this.panelUsuario.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -192,13 +241,12 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblolvidopassword;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button btnIngresar;
-        private System.Windows.Forms.Panel panelPassword;
-        private System.Windows.Forms.TextBox textBoxPassword;
-        private System.Windows.Forms.Panel panelUsuario;
-        private System.Windows.Forms.TextBox textBoxUsuario;
+        internal Klik.Windows.Forms.v1.EntryLib.ELButton btnIngresar;
+        internal Klik.Windows.Forms.v1.EntryLib.ELEntryBox textBoxUsuario;
+        internal Klik.Windows.Forms.v1.EntryLib.ELEntryBox textBoxPassword;
         private System.Windows.Forms.PictureBox pictureBoxOculo;
         private System.Windows.Forms.PictureBox pictureBoxVisible;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
